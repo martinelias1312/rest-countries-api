f7d402a77b1203132753468bab3287e87c65aa2d
Martin Eliáš <martinelias630@gmail.com>
2023-02-05 22:26:51 +0100

    develop: Added imports

--- rest-countries-api/src/app/app.module.ts
+++ rest-countries-api/src/app/app.module.ts
@@ -3,6 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CountryDetailModule } from './country-detail/country-detail.module';
+import { HomeModule } from './home/home.module';
+import { SharedModule } from './shared/shared.module';
 
 @NgModule({
   declarations: [
@@ -10,9 +13,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    HomeModule,
+    CountryDetailModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  exports: [SharedModule]
 })
 export class AppModule { }
