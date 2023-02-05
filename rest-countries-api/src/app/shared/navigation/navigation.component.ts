d742b5c1e81208ef3f1c1af3d1a7a4fda18e4d05
Martin Eliáš <martinelias630@gmail.com>
2023-02-05 19:00:36 +0100

    develop: Added basic navigation html

--- rest-countries-api/src/app/shared/navigation/navigation.component.ts
+++ rest-countries-api/src/app/shared/navigation/navigation.component.ts
@@ -7,9 +7,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavigationComponent implements OnInit {
 
+  public darkMode = false;
+
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  public onSetColorMode() {
+    this.darkMode = !this.darkMode;
+  }
 }
