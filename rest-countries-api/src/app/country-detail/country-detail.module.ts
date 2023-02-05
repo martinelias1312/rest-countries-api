a09f6631f0a9b6c2f673543f244dca8bd67ed672
Martin Eliáš <martinelias630@gmail.com>
2023-02-05 18:46:25 +0100

    develop: Added country-detail module and component

---
+++ rest-countries-api/src/app/country-detail/country-detail.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { CountryDetailComponent } from './country-detail.component';
+
+
+
+@NgModule({
+  declarations: [
+    CountryDetailComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CountryDetailModule { }
