2b3832ca9b6b77c6b24c6d5fd52f0678b056ea09
Martin Eliáš <martinelias630@gmail.com>
2023-02-06 21:57:04 +0100

    develop: Added country component

--- rest-countries-api/src/app/home/home.module.ts
+++ rest-countries-api/src/app/home/home.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { SharedModule } from '../shared/shared.module';
+import { CountryComponent } from './country/country.component';
 
 
 @NgModule({
   declarations: [
     HomeComponent,
+    CountryComponent,
   ],
   imports: [
     CommonModule,
